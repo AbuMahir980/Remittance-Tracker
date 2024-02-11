a2a221ace9359750c021c1f98cf2535ee31d83ca
Qudus Lawal <lawalqudus980@gmail.com>
2024-02-11 07:16:28 +0100

    Create Dashboard component + data file

---
+++ src/Components/Data/data.js
@@ -0,0 +1,12 @@
+export const initialTransactions = [
+    { id: 1, sender: 'Qudus', recipient: 'Abimbola', amount: 100, date: '2022-01-01', category: 'personal' },
+    { id: 2, sender: 'Abike', recipient: 'Adebola', amount: 200, date: '2022-01-02', category: 'business' },
+    { id: 3, sender: 'Fareedah', recipient: 'Lawal', amount: 300, date: '2022-01-03', category: 'personal' },
+    { id: 4, sender: 'Oyin', recipient: 'Ayokunle', amount: 400, date: '2022-01-04', category: 'business' },
+    { id: 5, sender: 'Lolade', recipient: 'Adun', amount: 500, date: '2022-01-05', category: 'personal' },
+    { id: 6, sender: 'Oyinladun', recipient: 'FarQud', amount: 600, date: '2022-01-06', category: 'business' },
+    { id: 7, sender: 'Rahmah', recipient: 'Adam', amount: 200, date: '2022-01-02', category: 'personal' },
+    { id: 8, sender: 'Shakir', recipient: 'Mariam', amount: 200, date: '2022-01-02', category: 'business' },
+    { id: 9, sender: 'Emmanuel', recipient: 'Jane', amount: 200, date: '2022-01-02', category: 'personal' },
+    { id: 10, sender: 'Betty', recipient: 'John Doe', amount: 200, date: '2022-01-02', category: 'business' }
+];
