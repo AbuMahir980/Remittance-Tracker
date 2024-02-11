c7c9092a713c202b15eb7e2ec31e0c5d164c4747
Qudus Lawal <lawalqudus980@gmail.com>
2024-02-11 07:54:52 +0100

    Add Footer component

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import '../../../css/footer.css'
+const Footer = () => {
+    return(
+
+        <footer className="footer">
+            <div className="footer_sign">made with <span style={{color:'red'}}>&#10084;</span> by <a href="https://github.com/AbuMahir980" target="blank" style={{}}>Abu Mahir</a>. Proudly <span style={{color:'lime'}}>🇳🇬</span></div>
+        </footer>
+    )
+}
+
+export default Footer
