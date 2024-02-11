7f6b5ed3e18166b2b025d2e0118b64800bc0df1b
Qudus Lawal <lawalqudus980@gmail.com>
2024-02-11 08:32:59 +0100

    Import AddTransaction component to Dashboard

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Header from '../Components/Header';
 import { Userprofile } from '../Components/Data/data';
 import Footer from '../Components/Footer';
+import AddTransaction from '../Components/AddTransaction';
 
 const Dashboard = () => {
     
     return (
         <div>
             <Header user={Userprofile}/>
+            <AddTransaction />
             <Footer />
         </div>
     )
