c0ccc9b7f7e306d91767ee1858609f5a7f046555
Qudus Lawal <lawalqudus980@gmail.com>
2024-02-11 08:05:47 +0100

    Style Footer elements

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,9 +1,9 @@
-import '../../../css/footer.css'
+
 const Footer = () => {
     return(
 
         <footer className="footer">
-            <div className="footer_sign">made with <span style={{color:'red'}}>&#10084;</span> by <a href="https://github.com/AbuMahir980" target="blank" style={{}}>Abu Mahir</a>. Proudly <span style={{color:'lime'}}>🇳🇬</span></div>
+            <div className="bg-blue-700 h-[10vh] flex justify-end items-center pr-10 text-white">made with <span className="text-red-500">&#10084;</span> by <a href="https://github.com/AbuMahir980" target="blank" className="text-yellow-300 text-base font-bold pl-1"> Abu Mahir</a>, Proudly <span className="text-[lime] pl-1 font-bold">🇳🇬</span>.</div>
         </footer>
     )
 }
