6f35b9ca4fe16e342a48b0bdd122755290c94db6
Qudus Lawal <lawalqudus980@gmail.com>
2024-02-11 07:57:38 +0100

    Import Footer component to dashboard

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../Components/Header';
 import { Userprofile } from '../Components/Data/data';
+import Footer from '../Components/Footer';
 
 const Dashboard = () => {
     
     return (
         <div>
             <Header user={Userprofile}/>
+            <Footer />
         </div>
     )
 }
