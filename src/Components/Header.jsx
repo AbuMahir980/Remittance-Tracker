43110ce8e38952df4238824d04f9c5af645bf328
Qudus Lawal <lawalqudus980@gmail.com>
2024-02-11 07:53:42 +0100

    Change username text color

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -6,7 +6,7 @@ const Header = (props) => {
     return(
         <>
             <div className="bg-blue-700 h-[10vh] flex justify-end items-center pr-10">
-                <p className="text-lg font-bold text-white">Hello <span className="text-[#FDBA0B]" >{user.userName}</span></p>
+                <p className="text-lg font-bold text-white">Hello, <span className="text-[#7be95a]" >{user.userName}</span></p>
             </div>
         </>
     )
