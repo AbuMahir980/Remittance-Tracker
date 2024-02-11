f2ee480d5e0fbace0f692cbeffc319c582d8cf1c
Qudus Lawal <lawalqudus980@gmail.com>
2024-02-11 08:53:11 +0100

    Create state to manage transactions

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../Components/Header';
 import { Userprofile } from '../Components/Data/data';
+import { initialTransactions } from '../Components/Data/data';
 import Footer from '../Components/Footer';
 import AddTransaction from '../Components/AddTransaction';
 
 const Dashboard = () => {
-    
+    const [transactions, setTransactions] = useState(initialTransactions);
+
+    const addTransaction = (transaction) => {
+        setTransactions([...transactions, transaction]);
+    };
     return (
         <div>
             <Header user={Userprofile}/>
-            <AddTransaction />
+            <div className='h-[80vh] flex flex-col justify-start items-center'>
+                <h1 className="text-center mt-5 mb-5 font-black text-3xl text-blue-900">Dashboard</h1>
+                <AddTransaction addTransaction={addTransaction}/>
+            </div>
             <Footer />
         </div>
     )
