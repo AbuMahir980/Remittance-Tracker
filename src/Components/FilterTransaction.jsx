1a09f7435a3ca9d30399106a931136192efb4461
Qudus Lawal <lawalqudus980@gmail.com>
2024-02-12 18:37:46 +0100

    Update changes

--- src/Components/FilterTransaction.jsx
+++ src/Components/FilterTransaction.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const FilterTransaction = ({setFilter}) => {
     return (
         <div>
-            <button onClick={() => setFilter('')}>All</button>
-            <button onClick={() => setFilter('personal')}>Personal</button>
-            <button onClick={() => setFilter('business')}>Business</button>
+            <button onClick={() => setFilter('')} className="p-1 pl-2 pr-2 rounded-md bg-blue-900 text-white font-bold">All</button>
+            <button onClick={() => setFilter('personal')} className="p-1 pl-2 pr-2 rounded-md bg-blue-900 text-white font-bold">Personal</button>
+            <button onClick={() => setFilter('business')} className="p-1 pl-2 pr-2 rounded-md bg-blue-900 text-white font-bold">Business</button>
         </div>
     )
 }
